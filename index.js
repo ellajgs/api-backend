bec9ab0ef615d9a9a4d5c04afa5ba70a346c2abd
ella <ejgsimmons@outlook.com>
2026-05-05 15:40:12 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello")
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send({"welcome": "hello world"})
+})
+
+app.get('/name', (req, res) => {
+    res.send(req.query)
+})
+
+app.get('/param/:id', (req,res)=>{
+    res.send(req.params)
+})
+
+app.listen(port, () => {
+    console.log(`simple api listening on port ${port}`)
+})
