c790789fd86a62abaa12af5f591b6cef9f214e26
ella <ejgsimmons@outlook.com>
2026-05-06 10:08:26 +0100

    add express.json function to app.js

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ app.get("/", (req, res) => {
   res.send("hello from the fruits api");
 });
 
-app.use("/fruits", fruits) //http://localhost:3000/fruits
+app.use(express.json()); //this needs to be above the other one
+// i guess bc its not got a route so it's applying to everything??
+app.use("/fruits", fruits); //http://localhost:3000/fruits
 
-module.exports = app
+module.exports = app;
